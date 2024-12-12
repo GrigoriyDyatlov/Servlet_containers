251296a68f25ce39521bdc9e38c77ad708e1b23c
Grigoriy Dyatlov <holy.crapicl@gmail.com>
2024-12-12 15:59:52 +0300

    add javaConfig

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,42 +1,16 @@
 package ru.netology.repository;
 
-import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
+public interface PostRepository {
+    List<Post> all();
 
-public class PostRepository {
-    protected final ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
-    private final AtomicLong idGenerator = new AtomicLong(0);
+    Optional<Post> getById(long id);
 
-    public List<Post> all() {
-        return posts.values().stream().toList();
-    }
+    Post save(Post post);
 
-    public Optional<Post> getById(long id) {
-        return Optional.ofNullable(posts.get(id));
-    }
-
-    public Post save(Post post) {
-        if (post.getId() == 0) {
-            post.setId(idGenerator.getAndIncrement());
-            posts.put(post.getId(), post);
-            return post;
-        } else if (post.getId() != 0) {
-            if (getById(post.getId()).isPresent()) {
-                getById(post.getId())
-                        .get()
-                        .setContent(post.getContent());
-            }
-            return post;
-        } else throw new NotFoundException();
-    }
-
-    public void removeById(long id) {
-        posts.remove(id);
-    }
+    void removeById(long id);
 }
